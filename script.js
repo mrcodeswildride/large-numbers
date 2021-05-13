32e793d4df1d3149cb63f2e45092c65c8e113727
David Keller <kellerdb@gmail.com>
2021-05-13 14:49:19 -0700

    formatting

--- script.js
+++ script.js
@@ -1,66 +1,72 @@
 let number = document.getElementById(`number`)
 let numberParagraph = document.getElementById(`numberParagraph`)
 
-let names = [``, `thousand`, `million`, `billion`, `trillion`, `quadrillion`, `quintillion`, `sextillion`, `septillion`, `octillion`, `nonillion`, `decillion`, `undecillion`, `duodecillion`, `tredecillion`, `quattuordecillion`, `quindecillion`, `sexdecillion`, `septendecillion`, `octodecillion`, `novemdecillion`, `vigintillion`]
+let names = [
+  ``,
+  `thousand`,
+  `million`,
+  `billion`,
+  `trillion`,
+  `quadrillion`,
+  `quintillion`,
+  `sextillion`,
+  `septillion`,
+  `octillion`,
+  `nonillion`,
+  `decillion`,
+  `undecillion`,
+  `duodecillion`,
+  `tredecillion`,
+  `quattuordecillion`,
+  `quindecillion`,
+  `sexdecillion`,
+  `septendecillion`,
+  `octodecillion`,
+  `novemdecillion`,
+  `vigintillion`,
+]
 
-number.addEventListener(`keydown`, keyPressed)
+number.addEventListener(`input`, spellNumber)
 number.focus()
-
-function keyPressed(event) {
-  if (event.keyCode == 13) {
-    spellNumber()
-  }
-}
 
 function spellNumber() {
   let numberValue = number.value.trim()
+  numberParagraph.innerHTML = ``
 
-  if (numberValue != `` && !isNaN(numberValue)) {
-    if (numberValue < 1) {
-      numberParagraph.innerHTML = `Number must be at least 1.`
-    }
-    else if (numberValue != Math.floor(numberValue)) {
-      numberParagraph.innerHTML = `Number must be an integer.`
-    }
-    else {
-      numberParagraph.innerHTML = ``
-      let count = 0
+  if (/^[0-9]+$/.test(numberValue)) {
+    let groupNum = 0
 
-      for (let i = numberValue.length - 3; i > -3; i -= 3) {
-        let group
+    for (let i = numberValue.length - 3; i > -3; i -= 3) {
+      let group
 
-        if (count < names.length - 1) {
-          group = numberValue.substring(i, i + 3)
-        }
-        else {
-          group = numberValue.substring(0, i + 3)
-        }
+      if (groupNum < names.length - 1) {
+        group = numberValue.substring(i, i + 3)
+      } else {
+        group = numberValue.substring(0, i + 3)
+      }
 
-        let trimmed = trimLeadingZeros(group)
+      let trimmed = trimLeadingZeros(group)
 
-        if (trimmed != ``) {
-          let numberText
-          
-          if (count == 0) {
-            numberText = trimmed
-          }
-          else {
-            numberText = `${trimmed} ${names[count]}`
-          }
+      if (trimmed != ``) {
+        let numberText
 
-          if (numberParagraph.innerHTML == ``) {
-            numberParagraph.innerHTML = numberText
-          }
-          else {
-            numberParagraph.innerHTML = `${numberText}, ${numberParagraph.innerHTML}`
-          }
+        if (groupNum == 0) {
+          numberText = trimmed
+        } else {
+          numberText = `${trimmed} ${names[groupNum]}`
         }
 
-        count++
-
-        if (count == names.length) {
-          break
+        if (numberParagraph.innerHTML == ``) {
+          numberParagraph.innerHTML = numberText
+        } else {
+          numberParagraph.innerHTML = `${numberText}, ${numberParagraph.innerHTML}`
         }
+      }
+
+      groupNum++
+
+      if (groupNum == names.length) {
+        break
       }
     }
   }
